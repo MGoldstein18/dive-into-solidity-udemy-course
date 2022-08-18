1c86476da9eb88c3d5da56c2cb4d3f56a01fbdb3
Mordi Goldstein <62645629+MGoldstein18@users.noreply.github.com>
2022-08-18 15:31:49 +0200

    completed all tasks

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -5,9 +5,34 @@ pragma solidity >=0.5.0 <0.9.0;
 contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {}
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address _address) public returns (bool, bytes memory) {
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool success, bytes memory returnBytes) = _address.call{value: address(this).balance}("");
+        return (success, returnBytes);
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata a, string calldata b) public pure returns (string memory) {
+        return string(abi.encodePacked(a, b));
     }
 }
